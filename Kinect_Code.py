126d4d504b440904b6fe277114b96f56f630e5e2
The-Rildot <61714377+The-Rildot@users.noreply.github.com>
2022-11-05 23:54:20 -0400

    Add files via upload
    uArm Kinect code and Library

---
+++ Kinect_Code.py
@@ -0,0 +1,150 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+from uarm.wrapper import SwiftAPI
+
+"""
+uArm code for movement
+moves based on the movement of the kinect, it doesn't let the user move past the allowed limit
+"""
+
+
+swift = SwiftAPI(filters={'hwid': 'USB VID:PID=2341:0042'}, cmd_pend_size=2, callback_thread_pool_size=1)
+
+swift.waiting_ready()
+
+device_info = swift.get_device_info()
+print(device_info)
+firmware_version = device_info['firmware_version']
+if firmware_version and not firmware_version.startswith(('0.', '1.', '2.', '3.')):
+    swift.set_speed_factor(0.00005)
+
+swift.set_mode(0)
+
+speed = 10000
+# x Limits: 110mm - 330mm
+# Y Limits: -350mm - 350mm
+# Z Limits: max 160mm
+# swift.reset(speed=speed)
+#  (138.54,115.64,148.39)|(195.62,115.64,148.39)|(285.21,115.64,148.39)
+# ---------------------------------------------------------------
+#  (138.54,3.07,148.39)|(195.62,3.07,148.39)|(285.21,3.07,148.39)
+# ---------------------------------------------------------------
+#  (138.54,-115.64,148.39)|(195.62,-115.64,148.39)|(285.21,-115.64,148.39)
+# y+:x+,x0,x- -> y0:x+,x0,x- -> y-:x+,x0,x- --> z
+
+x_max = 285.21
+x_min = 138.54
+y_max = 115.64
+y_min = -115.64
+z_max = 156.13
+z_min = 83
+# base start
+swift.reset()
+swift.get_position(wait=False, callback=lambda i: print('pos', i))
+
+while swift.connected:
+    x_pos = 195.62
+    y_pos = 3.07
+    z_pos = 148.39
+    if x_pos > x_max:
+        x_pos = x_max
+    elif x_pos < x_min:
+        x_pos = x_min
+    if y_pos > y_max:
+        y_pos = y_max
+    elif y_pos < y_min:
+        y_pos = y_min
+    if z_pos > z_max:
+        z_pos = z_max
+    elif z_pos < z_min:
+        z_pos = z_min
+    # move based on hand
+    swift.set_position(x=x_pos, y=y_pos, z=z_pos, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+"""
+    # TOP
+    # Position 1
+    swift.set_position(x=285.21, y=115.64, z=156.13, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 2
+    swift.set_position(x=195.62, y=115.64, z=156.13, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 3
+    swift.set_position(x=138.54, y=115.64, z=156.13, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 4
+    swift.set_position(x=285.21, y=3.07, z=156.13, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 5(center)
+    swift.set_position(x=195.62, y=3.07, z=156.13, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 6
+    swift.set_position(x=138.54, y=3.07, z=156.13, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 7
+    swift.set_position(x=285.21, y=-115.64, z=156.13, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 8
+    swift.set_position(x=195.62, y=-115.64, z=156.13, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 9
+    swift.set_position(x=138.54, y=-115.64, z=156.13, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # MIDDLE
+    # Position 1
+    swift.set_position(x=285.21, y=115.64, z=148.39, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 2
+    swift.set_position(x=195.62, y=115.64, z=148.39, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 3
+    swift.set_position(x=138.54, y=115.64, z=148.39, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 4
+    swift.set_position(x=285.21, y=3.07, z=148.39, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 5(center)
+    swift.set_position(x=195.62, y=3.07, z=148.39, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 6
+    swift.set_position(x=138.54, y=3.07, z=148.39, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 7
+    swift.set_position(x=285.21, y=-115.64, z=148.39, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 8
+    swift.set_position(x=195.62, y=-115.64, z=148.39, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 9
+    swift.set_position(x=138.54, y=-115.64, z=148.39, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # BOTTOM
+    # Position 1
+    swift.set_position(x=285.21, y=115.64, z=83, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 2
+    swift.set_position(x=195.62, y=115.64, z=83, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 3
+    swift.set_position(x=138.54, y=115.64, z=83, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 4
+    swift.set_position(x=285.21, y=3.07, z=83, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 5(center)
+    swift.set_position(x=195.62, y=3.07, z=83, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 6
+    swift.set_position(x=138.54, y=3.07, z=83, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 7
+    swift.set_position(x=285.21, y=-115.64, z=83, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 8
+    swift.set_position(x=195.62, y=-115.64, z=83, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+    # Position 9
+    swift.set_position(x=138.54, y=-115.64, z=83, speed=speed)
+    swift.get_position(wait=False, callback=lambda i: print('pos', i))
+"""
